83b5aef4e2b8f0e1cac5a40b63dc92cf504f2ad5
Rishav Baral <rishavbaral6@gmail.com>
2020-05-27 22:29:08 +0000

    add countOnly.js function

--- random.js
+++ random.js
@@ -1,6 +1,15 @@
-var globalScope = 'I\'m all up in your codes!';
+var planetMoons = {
+  mercury: 0,
+  venus: 0,
+  earth: 1,
+  mars: 2,
+  jupiter: 67,
+  saturn: 62,
+  uranus: 27,
+  neptune: 14
+};
 
-if (true) {
-  globalScope = 'Not anymore, you\'re not!';
+for (var planet in planetMoons) {
+  var numberOfMoons = planetMoons[planet];
+  console.log("Planet: " + planet + ", # of Moons: "+ numberOfMoons);
 }
-console.log(globalScope);
