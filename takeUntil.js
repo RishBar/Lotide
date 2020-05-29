e31fcc752c7982836f037733e9a972efcb50f9e9
Rishav Baral <rishavbaral6@gmail.com>
2020-05-29 06:53:57 +0000

    add takeUntil.js, function which will take two parameters, an array, and a callback function, takeUntil will interate through the array and call the callback function with each element of the array as a parameter and it will add the element to a new array if until the callback function returns a truthy value. once the callb ack function recieves a truthy value, return the array'

---
+++ takeUntil.js
@@ -0,0 +1,37 @@
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    console.log(`❌❌❌ Assertion failed: [${arrayOne}] !== [${arrayTwo}]`);
+    return;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      console.log(`❌❌❌ Assertion failed: [${arrayOne}] !== [${arrayTwo}]`);
+      return;
+    }
+  }
+  console.log(`✅✅✅ Assertion passed: [${arrayOne}] === [${arrayTwo}]`);
+};
+
+
+const takeUntil = function(arr, cb) {
+  const newArr = [];
+  for (const i of arr) {
+    if (cb(i)) {
+      return newArr;
+    }
+    newArr.push(i);
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertArraysEqual([1, 2, 5, 7, 2], results1);
+assertArraysEqual(["I've", "been", "to", "Hollywood"], results2);
