84f271513171e92b6222b9713a5f9728c077c185
Rishav Baral <rishavbaral6@gmail.com>
2020-06-01 15:11:17 -0700

    refactor code by exporting and requiring different reusable functions

--- assertEqual.js
+++ assertEqual.js
@@ -6,8 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("yo", "yo");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(2, 1);
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+
+// Assert Equal TEST CODE
+assertEqual("yo", "yo");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(2, 1);
